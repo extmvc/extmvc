d2938fc02840a24c5eb056bb296e3d22a1016f80
Ed Spencer <edward@midlandswebdesign.com>
2008-04-16 20:22:39 +0100

    Removed duplicate rspec_ext_scaffold generator... oops

--- generators/ext_mvc/templates/lib/generators/rspec_ext_scaffold/templates/models/_model.js
+++
@@ -1,8 +0,0 @@
-var <%= class_name %>Record = Ext.data.Record.create([
-  { name: 'id',  mapping: '<%= file_name %>.id', type: 'int'},
-<%= field_collection.fields.collect {|f| "  { name: '#{f.field_name}', mapping: '#{file_name}.#{f.field_name}', type: 'string'}"}.join(",\n") %>
-]);
-
-var <%= file_name %>Reader = new Ext.data.JsonReader({root: '<%= table_name %>',totalProperty: 'results'}, <%= class_name %>Record);
-var <%= class_name %> = {model_name : '<%= file_name %>', human_name: '<%= class_name %>', plural_name: '<%= file_name.pluralize %>'};
-Ext.apply(<%= class_name %>, Model);
