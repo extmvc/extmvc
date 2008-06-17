c20d0ca5fda5dbccf85530c0acec776633736d67
Edward Spencer <edward@midlandswebdesign.com>
2008-06-17 12:15:31 +0100

    Added version, start of an In Place has_many grid

--- base.js
+++ base.js
@@ -2,3 +2,6 @@ Ext.namespace("Ext.ux.MVC", "Ext.ux.MVC.controller", "Ext.ux.MVC.model", "Ext.ux
 
 //additional helper namespaces
 Ext.namespace("Ext.ux.MVC.helper.button", "Ext.ux.MVC.helper.grid", "Ext.ux.MVC.helper.form");
+
+//version 0.4
+Ext.ux.MVC.version = "0.4";
