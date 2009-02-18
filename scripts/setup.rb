11b8af50a997bca62e50d70629ccf748016e4f57
Edward Spencer <edward@midlandswebdesign.com>
2009-02-18 17:20:30 +0000

    Added setup script

---
+++ scripts/setup.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'fileutils'
+
+module ExtMVC
+  # overwrites all instances of 'MyApp' with the namespace provided
+  def self.setup(namespace)
+    files = %w(
+      app/OS.js
+      app/controllers/IndexController.js
+      app/views/index/Index.js
+      config/initialize.js
+      config/settings.yml
+    )
+    
+    files.each {|f| gsub_file(f, 'MyApp', namespace)}
+  end
+  
+  private
+  
+  #find/replace in a file.  Stolen/adapted from Rails
+  def self.gsub_file(filename, regexp, *args, &block)
+    content = File.read(filename).gsub(regexp, *args, &block)
+    File.open(filename, 'wb') { |file| file.write(content) }
+  end
+end
